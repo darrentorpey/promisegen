68694d3af97ea9e5a9baa98afbe773eaaef02c8d
Darren Torpey <darren.torpey@gmail.com>
2016-09-06 17:52:22 -0700

    Handling error where reducer returns a non-number

--- src/reducer.js
+++ src/reducer.js
@@ -3,23 +3,42 @@ function reduceByPercent(num, percent) {
     return num * (1 - ((percent/100)));
 }
 
+function makeReductionGenerator(reducer) {
+    return function* reductionGenerator(num=100) {
+        var new_num = num;
+
+        do {
+            yield new_num;
+
+            num = new_num;
+
+            let new_num_raw = reducer(num);
+
+            if (typeof(new_num_raw) !== 'number') {
+                console.error(new_num_raw);
+                throw Error('Reducer returned a non-number result');
+            }
+
+            new_num = Math.round(new_num_raw);
+        } while (num != new_num)
+    };
+}
+
 class Reducer {
-    static create({ percent }) {
+    static create({ percent } = {}) {
         function reducer(num) {
             return reduceByPercent(num, percent);
         }
 
-        return function* reductionGenerator(num=100) {
-            var new_num = num;
+        return makeReductionGenerator(reducer);
+    }
 
-            do {
-                yield new_num;
+    static createAsync({ percent } = {}) {
+        function reducer(num) {
+            return new Promise.resolve(reduceByPercent(num, percent));
+        }
 
-                num = new_num;
-
-                new_num = Math.round(reducer(num));
-            } while (num != new_num)
-        };
+        return makeReductionGenerator(reducer);
     }
 
     static reduceDown({ percent }) {
